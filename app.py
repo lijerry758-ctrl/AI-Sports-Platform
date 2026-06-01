0e740c1f52111b496fb330e810814afa117df74b
George <lijerry758@gmail.com>
2026-06-01 20:21:59 +0800

    Add detection page and connect with home page

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# 這是我們網站的主頁路由
+# 1. 這是我們網站的主頁
 @app.route('/')
 def home():
-    # 讓 Flask 去 templates 資料夾找 home.html 並渲染出來
     return render_template('home.html')
 
+# 2. 這是新加的：AI 運動偵測頁面
+@app.route('/detection')
+def detection():
+    # 讓 Flask 去 templates 資料夾找 detection.html 並渲染出來
+    return render_template('detection.html')
+
 if __name__ == '__main__':
-    # 啟動本地測試伺服器，並開啟 debug 模式方便排錯
     app.run(debug=True)
