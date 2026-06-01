2871d7e760d508c3ea540c657718e35b0cee1f4f
George <lijerry758@gmail.com>
2026-06-01 21:01:01 +0800

    Prepare for cloud deployment

--- app.py
+++ app.py
@@ -14,4 +14,5 @@ def detection():
     return render_template('detection.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # 讓同一個 Wi-Fi 底下的手機平板都能連進來
+    app.run(host='0.0.0.0', port=5000, debug=True)
